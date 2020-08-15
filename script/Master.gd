81a6b938a9c09a0c8ca58d39044f4d59623b78c1
Robbo <69299134+nduja@users.noreply.github.com>
2020-08-15 19:36:41 +1000

    Add proof of concept pet bouncing. (#3)
    * Adding empty scene for Pet entity.
    
    * Add proof of concept for pet bouncing

--- script/Master.gd
+++ script/Master.gd
@@ -2,6 +2,7 @@ extends Node
 
 const Intro = preload("res://scene/Intro.tscn")
 const Menu = preload("res://scene/Menu.tscn")
+const PettingGame = preload("res://scene/PettingGame.tscn")
 
 onready var current_scene = get_node("Title")
 
@@ -26,4 +27,11 @@ func load_menu():
 	var menu_scene = Menu.instance()
 	add_child(menu_scene)
 	current_scene = menu_scene
+	return
+
+func load_petting_game():
+	remove_current_scene()
+	var petting_game_scene = PettingGame.instance()
+	add_child(petting_game_scene)
+	current_scene = petting_game_scene
 	return
---
+++ script/PettingGame.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+onready var pet = get_node("res://scene/Pet.tscn")
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
