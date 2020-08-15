2f61beb0ab9ffa99aa7ae1b78c2053b54082098f
Liam <liam.s.oconnell@gmail.com>
2020-08-15 19:56:16 +1000

    Modified UI

--- script/NeedBar.gd
+++ script/NeedBar.gd
@@ -1,16 +1,22 @@
 extends Control
 
 const MiddleSegment = preload("res://scene/NeedBarMiddle.tscn")
+const EndSegment = preload("res://scene/NeedBarEnd.tscn")
 
-#var segment_number : int
-var segment_number = 12
+#Max should be 8
+var segment_number = 4
 
 func _ready():
-	if(segment_number > 2):
-		var container = get_node("HBoxContainer")
-		var start = container.get_node("Start")
+	var container = get_node("HBoxContainer")
+	var start = container.get_node("Start")
+	if(segment_number > 1):
 		for i in segment_number-2:
 			var middle_segment = MiddleSegment.instance()
 			middle_segment.expand = true
-			container.add_child_below_node(start, middle_segment)
+			container.add_child(middle_segment)
+		var end_segment = EndSegment.instance()
+		end_segment.expand = true
+		container.add_child(end_segment)
+		
+	container.margin_right = segment_number * 64
 	return
