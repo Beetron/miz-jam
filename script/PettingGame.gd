889d0e681b7c72274180f4b193d630d418b4c705
Liam <liam.s.oconnell@gmail.com>
2020-08-16 14:27:19 +1000

    Spelling mistake

--- script/PettingGame.gd
+++ script/PettingGame.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 signal petting_won(need)
-signal peting_lost
+signal petting_lost
 
 var difficulty = 2
 var clicks = 0
 
 func _ready():
 	self.connect("petting_won", get_parent(), "won_game")
-	self.connect("peting_lost", get_parent(), "return_to_menu")
+	self.connect("petting_lost", get_parent(), "return_to_menu")
 	return
 
 func _process(delta):
